16812859f67b80e9db2f8e2fcb1f26dc33e83cca
tamakiii <tamakiii@users.noreply.github.com>
2020-02-09 23:43:46 +0900

    cleanup

--- crawler/src/parse.js
+++ crawler/src/parse.js
@@ -36,32 +36,14 @@ if (options.verbose) {
 
 const puppeteer = require('puppeteer-core');
 
-(async() => {
-  const browser = await puppeteer.launch({
-    executablePath: options.executable,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--diable-gpu',
-      options.address ? `--remote-debugging-address=${options.address}` : undefined,
-      options.port ? `--remote-debugging-port=${options.port}` : undefined,
-      '--lang=ja-jp,jp',
-    ].filter(e => e)
-  });
-
-  const page = await browser.newPage();
-  await page.setContent(input);
-  // await page.goto('https://www.google.co.jp/');
-  // await page.screenshot({
-  //   path: 'example.png'
-  // });
-
-  const jsonize = function(nodes) {
+class __Custom__ {
+  static jsonize(nodes) {
     if (nodes.childElementCount > 0) {
+      console.log(this.jsonize);
       return {
         name: nodes.nodeName,
         body: Array.from(nodes.childNodes)
-          .map(node => jsonize(node))
+          .map(node => this.jsonize(node))
           .filter(x => x)
       }
     }
@@ -88,44 +70,24 @@ const puppeteer = require('puppeteer-core');
       name: node.nodeName,
       body: node.textContent,
     }
-  };
+  }
+}
 
-  class __Custom__ {
-    static jsonize(nodes) {
-      if (nodes.childElementCount > 0) {
-        console.log(this.jsonize);
-        return {
-          name: nodes.nodeName,
-          body: Array.from(nodes.childNodes)
-            .map(node => this.jsonize(node))
-            .filter(x => x)
-        }
-      }
+(async () => {
+  const browser = await puppeteer.launch({
+    executablePath: options.executable,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--diable-gpu',
+      options.address ? `--remote-debugging-address=${options.address}` : undefined,
+      options.port ? `--remote-debugging-port=${options.port}` : undefined,
+      '--lang=ja-jp,jp',
+    ].filter(e => e)
+  });
 
-      const node = nodes;
-
-      if (node.nodeName === '#text') {
-        if (node.wholeText.trim().length === 0) {
-          return undefined;
-        }
-        return {
-          name: node.nodeName,
-          body: node.wholeText.trim(),
-        }
-      } else if (node.nodeName === 'A') {
-        return {
-          name: node.nodeName,
-          body: node.textContent,
-          href: node.href,
-        };
-      }
-
-      return {
-        name: node.nodeName,
-        body: node.textContent,
-      }
-    }
-  };
+  const page = await browser.newPage();
+  await page.setContent(input);
 
   await page.addScriptTag({
     content: __Custom__.toString()
@@ -136,8 +98,8 @@ const puppeteer = require('puppeteer-core');
   evaluates.push(
     page.evaluate(() => {
       const title = Array.from(
-          document.querySelector("#main > div:nth-child(1) > div.group_inner.event_header_area > h2").childNodes
-        )
+        document.querySelector("#main > div:nth-child(1) > div.group_inner.event_header_area > h2").childNodes
+      )
         .filter(node => node.nodeName === '#text')
         .map(node => node.wholeText)
         .join('')
@@ -149,39 +111,23 @@ const puppeteer = require('puppeteer-core');
     })
   );
 
-  page.evaluate(() => {}, 'hoge');
+  evaluates.push(
+    page.evaluate(() => {
+      const main = document.querySelector('#main > div:nth-child(3) div#editor_area');
 
-  const getDetail = () => {
-    const main = document.querySelector('#main > div:nth-child(3) div#editor_area');
-
-    return {
-      detail: __Custom__.jsonize(main)
-    };
-  };
-
-  evaluates.push(page.evaluate(getDetail));
+      return {
+        detail: __Custom__.jsonize(main)
+      };
+    })
+  );
 
   Promise.all(evaluates)
     .then(results => {
-      // console.log('ok');
       console.log(JSON.stringify(results));
     })
     .catch(error => {
       console.log(error);
-    })
-    .finally(async () => {
-      // await browser.close();
     });
-
-  // page.on("close", async (e, args) => {
-  //   await browser.close();
-  //   console.log('done(success)');
-  // });
-
-  // page.on("request", (request, args) => {
-  //   console.log('on request');
-  //   console.log(request, args);
-  // });
 
   setTimeout(async () => {
     await browser.close();
