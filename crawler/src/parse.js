be1230dbe04d2051ea6d04d165b541f95bf91183
tamakiii <tamakiii@users.noreply.github.com>
2020-02-09 23:38:17 +0900

    impl parse title and detail using addScriptTag()

--- crawler/src/parse.js
+++ crawler/src/parse.js
@@ -28,8 +28,11 @@ const options = require('command-line-args')([
   }
 ]);
 
-// const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-// process.stdout.write(input);
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+
+if (options.verbose) {
+  process.stdout.write(input);
+}
 
 const puppeteer = require('puppeteer-core');
 
@@ -42,24 +45,143 @@ const puppeteer = require('puppeteer-core');
       '--diable-gpu',
       options.address ? `--remote-debugging-address=${options.address}` : undefined,
       options.port ? `--remote-debugging-port=${options.port}` : undefined,
+      '--lang=ja-jp,jp',
     ].filter(e => e)
   });
 
   const page = await browser.newPage();
-  await page.goto('https://www.google.co.jp/');
-  await page.screenshot({
-    path: 'example.png'
-  });
+  await page.setContent(input);
+  // await page.goto('https://www.google.co.jp/');
+  // await page.screenshot({
+  //   path: 'example.png'
+  // });
 
-  page.on("close", async (e, args) => {
-    await browser.close();
-    console.log('done(success)');
-  });
+  const jsonize = function(nodes) {
+    if (nodes.childElementCount > 0) {
+      return {
+        name: nodes.nodeName,
+        body: Array.from(nodes.childNodes)
+          .map(node => jsonize(node))
+          .filter(x => x)
+      }
+    }
 
-  page.on("request", (request, args) => {
-    console.log('on request');
-    console.log(request, args);
-  });
+    const node = nodes;
+
+    if (node.nodeName === '#text') {
+      if (node.wholeText.trim().length === 0) {
+        return undefined;
+      }
+      return {
+        name: node.nodeName,
+        body: node.wholeText.trim(),
+      }
+    } else if (node.nodeName === 'A') {
+      return {
+        name: node.nodeName,
+        body: node.textContent,
+        href: node.href,
+      };
+    }
+
+    return {
+      name: node.nodeName,
+      body: node.textContent,
+    }
+  };
+
+  class __Custom__ {
+    static jsonize(nodes) {
+      if (nodes.childElementCount > 0) {
+        console.log(this.jsonize);
+        return {
+          name: nodes.nodeName,
+          body: Array.from(nodes.childNodes)
+            .map(node => this.jsonize(node))
+            .filter(x => x)
+        }
+      }
+
+      const node = nodes;
+
+      if (node.nodeName === '#text') {
+        if (node.wholeText.trim().length === 0) {
+          return undefined;
+        }
+        return {
+          name: node.nodeName,
+          body: node.wholeText.trim(),
+        }
+      } else if (node.nodeName === 'A') {
+        return {
+          name: node.nodeName,
+          body: node.textContent,
+          href: node.href,
+        };
+      }
+
+      return {
+        name: node.nodeName,
+        body: node.textContent,
+      }
+    }
+  };
+
+  await page.addScriptTag({
+    content: __Custom__.toString()
+  })
+
+  const evaluates = [];
+
+  evaluates.push(
+    page.evaluate(() => {
+      const title = Array.from(
+          document.querySelector("#main > div:nth-child(1) > div.group_inner.event_header_area > h2").childNodes
+        )
+        .filter(node => node.nodeName === '#text')
+        .map(node => node.wholeText)
+        .join('')
+        .trim();
+
+      return {
+        title
+      };
+    })
+  );
+
+  page.evaluate(() => {}, 'hoge');
+
+  const getDetail = () => {
+    const main = document.querySelector('#main > div:nth-child(3) div#editor_area');
+
+    return {
+      detail: __Custom__.jsonize(main)
+    };
+  };
+
+  evaluates.push(page.evaluate(getDetail));
+
+  Promise.all(evaluates)
+    .then(results => {
+      // console.log('ok');
+      console.log(JSON.stringify(results));
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(async () => {
+      // await browser.close();
+    });
+
+  // page.on("close", async (e, args) => {
+  //   await browser.close();
+  //   console.log('done(success)');
+  // });
+
+  // page.on("request", (request, args) => {
+  //   console.log('on request');
+  //   console.log(request, args);
+  // });
 
   setTimeout(async () => {
     await browser.close();
